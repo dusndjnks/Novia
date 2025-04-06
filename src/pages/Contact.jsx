d51f573c149abbc747e056d3acae1b7efcb295b5
dusndjnks <mhdaffan0000@gmail.com>
2025-04-06 15:08:02 +0530

    x

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,78 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+          <div className="text-center pt-12 md:pb-12 px-6 mt-12">
+    <h1 className="font-display font-bold text-5xl mb-6 font-serif text-gray-700 ">Get in Touch</h1>
+  </div>
+
+  <div className="container mx-auto px-6 mb-12">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 max-w-4xl mx-auto">
+      <div>
+        <a href="tel:+1234567890" className="flex items-center my-2">
+          <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"
+              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+          </svg>
+          +1234567890
+        </a>
+
+        <a href="mailto:hello@example.com" className="flex items-center my-2">
+          <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+          </svg>
+          hello@example.com
+        </a>
+
+        <div className="flex items-center my-2">
+          <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          Mon - Fri: 9am - 6pm
+        </div>
+
+        <a href="https://www.google.com/maps/place/123 Wedding Lane, Lovelytown"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center my-2">
+          <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"
+              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"
+              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+          123 Wedding Lane, Lovelytown
+        </a>
+      </div>
+
+      <div>
+        <form method="POST" name="contact">
+          <div>
+            <label className="block text-base tracking-tight text-gray-600">Name</label>
+            <input name="name" type="text" placeholder="Name" required
+              className="mt-2 w-full border-none text-sm p-4 bg-gray-100 text-gray-700" />
+          </div>
+          <div className="mt-6">
+            <label className="block text-base tracking-tight text-gray-600">Email address</label>
+            <input name="email" type="email" placeholder="your@email.com" required
+              className="mt-2 w-full border-none text-sm p-4 bg-gray-100 text-gray-700" />
+          </div>
+          <div className="mt-6">
+            <label className="block text-base tracking-tight text-gray-600">Message</label>
+            <textarea name="message" placeholder="Your message" required
+              className="mt-2 w-full border-none text-sm p-4 bg-gray-100 text-gray-700"></textarea>
+          </div>
+          <button type="submit"
+            className="inline-block bg-[#3a4861] text-white uppercase text-sm tracking-widest font-heading px-8 py-4 mt-6">
+            Send message
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+    </div>
   )
 }
 
